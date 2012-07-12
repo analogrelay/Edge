a3d1dda15611d918dfd3dbd095e9b00522a062d9
anurse <andrew@andrewnurse.net>
2012-07-12 15:34:45 -0700

    Starting to update to Katana

--- LaunchEdgeDemo.cmd
+++ LaunchEdgeDemo.cmd
@@ -1,3 +1,6 @@
+pushd %~dp0
+%systemroot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+popd
 pushd %~dp0EdgeDemo
-..\packages\Ghost.0.3.4\bin\Ghost.exe -s firefly -v
+..\packages\Ghost.0.3.4\bin\Ghost.exe -v
 popd
